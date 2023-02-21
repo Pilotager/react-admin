3575cb97cbba7f6b7a0097654c337cba8dcc8ecf
Pilotager <yin.bighang@gmail.com>
2023-02-21 23:13:27 +0800

    feat: 🎸 关联tagStore

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,1 +1,9 @@
-export class RootStore {}
+import { TagStore } from './TagStore';
+
+export class RootStore {
+  public tagStore: TagStore;
+
+  constructor() {
+    this.tagStore = new TagStore();
+  }
+}
